aff2c12d323e745334a5d14684142a8b0e934b2e
Yaro Williams <yarowilliams402@gmail.com>
2025-03-18 12:40:44 +0000

    Exercises or Challenge completed

---
+++ YaroWilliams/Exercise2.java
@@ -0,0 +1,9 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        // Exercise2 --Debugging--
+        System.out.println("Day1 - String Manipulation");
+        System.out.println("String Concatenation is done with the '+' sign.");
+        System.out.println("e.g."+ "Hello" + "world");
+        System.out.println("New lines can be created with a backslash and n.");
+    }
+}
