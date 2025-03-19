36670084117e6ed058268226453d74e7620096fe
Williams <yarowilliams402@gmail.com>
2025-03-19 11:43:45 +0000

    A small change

--- YaroWilliams/Exercise7.java
+++ YaroWilliams/Exercise7.java
@@ -4,7 +4,7 @@ public class Exercise7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("What is your current age? ");
-        int age = input.nextInt();
+        int age = input.nextInt();// enter age
         int yearsLeft = MAX_AGE - age;
         int daysLeft = yearsLeft * 365;
         int weeksLeft = yearsLeft * 52;
