dec9a0abd0daf45e16dd2ae2a93999222051c720
Prince Eli. <peamuzu003@st.ug.edu.gh>
2025-03-19 02:07:29 +0000

    Day 1 challenges (#11)

---
+++ Sunset/Basics/CODE_CHALLENGES/Day_1/exercise1.js
@@ -0,0 +1,10 @@
+/*Instructions:
+Write a program to display the output below exactly as it is:
+
+Day 1 - Output Function
+The function is declared like this:
+print('what to print')*/
+
+console.log(`Day 1 - Output Function
+The function is declared like this:
+print('what to print')`)
