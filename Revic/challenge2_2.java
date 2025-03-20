7f2531f519a2f2804361fe4cb71cf7464c3267a4
r.owusu <revicowusu@gmail.com>
2025-03-20 19:43:24 +0000

    update

--- Revic/challenge2_2.java
+++ Revic/challenge2_2.java
@@ -7,13 +7,29 @@ public class challenge2_2 {
      Scanner sc = new Scanner(System.in);
      System.out.println("your weight is:");
 
-int weight=sc.nextInt();
+double weight=sc.nextDouble();
 System.out.println("your height is:");
-int height=sc.nextInt();
-int bmi=weight/(height*height);
+double height=sc.nextDouble();
+double fbmi=weight/(height*height);
+int bmi= (int)fbmi;
 System.out.println("your weight is:"+ weight + "kg");
 System.out.println("your height is:"+height + "m");
 System.out.println("your Body Mass Index is:" + bmi);
+if (bmi<18.5){
+    System.out.println("You are underweight");}
+else if(bmi >= 18.5 && bmi<=24.9){
+        System.out.println("Your weight is normal");
+    }
+else if(bmi>=25 && bmi<=29.99){
+        System.out.println("You are overweight");
+    }
+else if(bmi>=30 && bmi <=34.9){
+    System.out.println("You are Obese");
+}
+else{
+    System.out.println("You are clnically obese");
+}
+
     }
     
 }
