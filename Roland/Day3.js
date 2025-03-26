f0dbc645d16098227fadf94332a753fb66ecc757
Roland Appiah <romappiah@st.ug.edu.gh>
2025-03-26 13:26:44 +0000

    Java script programming b4 (#37)
    * Day1 Exercise1
    
    * Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 1
    
    * Day 1 Exercise 1
    
    * git Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 4 solved
    
    * Day 2 Exercise 1 Solved
    
    * Day 2 Exercise 6
    
    * Exercise 9 solved
    
    * Exercise 10 solved
    
    * Exercise 11 Solved with a smile

--- Roland/Day3.js
+++ Roland/Day3.js
@@ -18,3 +18,30 @@ function isLeapYear(year){
         console.log("Year is a leap year")
     }
 }
+
+
+//Exercise 11
+console.log("Welcome to Papa's Pizza");
+const size = prompt("What size pizza do you want? S, M, or L");
+const pepperoni = prompt("Do you want pepperoni? Y or N");
+const cheese = prompt("Do you want extra cheese? Y of N");
+let bill = 0;
+
+if (size === "S"){
+    bill += 15;
+}else if (size === "M"){
+    bill += 20;
+}else if (size === "L"){
+    bill += 25;
+}
+if ((bill === 20 || bill === 25) && pepperoni === "Y"){
+    bill += 3;
+} else if (bill === 15 && pepperoni === "Y"){
+    bill += 2;
+}
+if (cheese === "Y"){
+    bill += 1;
+}
+
+console.log("Your final bill is $"+ bill);
+
