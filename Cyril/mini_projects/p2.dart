57fd1aa3f064d95978a95fb5d570bd4900a1c612
Cyril-Nii <cyriltagoe0@gmail.com>
2025-03-28 10:50:28 +0000

    One more p done

--- Cyril/mini_projects/p2.dart
+++ Cyril/mini_projects/p2.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 void main() {
   print("Welcome to the Tip Calculator!");
   stdout.write("What was the total bill: \$");
-  double totalBill = double.parse(stdin.readLineSync() ?? "0");
+  double totalBill = double.parse(stdin.readLineSync()!);
 
   stdout.write("What percentage tip would you like to give? 10, 12, or 15: ");
-  int tipPercentage = int.parse(stdin.readLineSync() ?? "0");
+  int tipPercentage = int.parse(stdin.readLineSync()!);
 
   stdout.write("How many people to split the bill? ");
-  int people = int.parse(stdin.readLineSync() ?? "0");
+  int people = int.parse(stdin.readLineSync()!);
 
   double tip = totalBill * (tipPercentage / 100);
   double total = totalBill + tip;
