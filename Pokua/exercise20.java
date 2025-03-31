96a8eb9649c9480cd5cfc4e55cab84d33070e2de
Adwoa Aboagye <adwoa.aboagye@turntabl.io>
2025-03-31 01:55:55 +0000

    completed exercises

---
+++ Pokua/exercise20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exercise20 {
+
+    public void checker(){
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+    }
+
+    public static void main(String[] args){
+
+        exercise20 obj = new exercise20();
+
+        obj.checker();
+    }
+}
