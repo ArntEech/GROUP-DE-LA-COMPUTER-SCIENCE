5ff3e683ff1e8ff5b0fc9c9ce3afe2284496a85b
Arnt <Arnatech5@gmail.com>
2025-04-02 10:14:32 +0000

    Exercise18.java

--- Pokua/Exercise18.java
+++ Pokua/Exercise18.java
@@ -1,3 +1,12 @@
 public class Exercise18 {
     
+    public static void main(String[] args){
+        int sum = 0;
+
+        for(int i=0; i<=100; i+=2){
+            sum+=i;
+        }
+
+        System.out.println("The sum from 1 to 100 is "+sum);
+    }
 }
