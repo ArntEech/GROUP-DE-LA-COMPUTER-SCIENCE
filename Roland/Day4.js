8dd41af62eb33a141cd32e17cb1fe458896d068a
rolandhimself <romappiah@st.ug.edu.gh>
2025-04-02 09:40:12 +0100

    Day 4 completed

--- Roland/Day4.js
+++ Roland/Day4.js
@@ -17,14 +17,23 @@ console.log(`${randomName()} is paying today`);
 
 //Exercise15
 
-const row1 = ["⬜️","⬜️","⬜️"];
-
-const row2 = ["⬜️","⬜️","⬜️"];
-
-const row3 = ["⬜️","⬜️","⬜️"];
+const row1 = ["😙","😙","😙"];
+const row2 = ["😙","😙","😙"];
+const row3 = ["😙","😙","😙"];
 
 const map = [row1, row2, row3];
 
 console.log(`${row1}\n${row2}\n${row3}`);
+const position = prompt("Where do you want to put the treasure? \n");
 
-const position = prompt("Where do you want to put the treasure? ");
+if (position > 33){
+    throw new Error("Input should follow a 3x3 matrix");
+}
+let column = parseInt(position[0]) - 1;
+let row = parseInt(position[1]) - 1;
+
+if (column >= 0 && column < 3 && row >= 0 && row < 3) {
+  map[row][column] = "❌";}
+
+console.log("Updated Map:");
+map.forEach(row => console.log(row.join(" ")));
