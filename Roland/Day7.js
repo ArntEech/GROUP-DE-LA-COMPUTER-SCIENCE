b00d1b8fd16d4b07511701da354df788d6469de4
rolandhimself <romappiah@st.ug.edu.gh>
2025-04-03 13:32:29 +0100

    Prime number logic complete

--- Roland/Day7.js
+++ Roland/Day7.js
@@ -6,3 +6,32 @@ function calculate(){
     return Math.ceil((height * width)/5);
 }
 console.log(`You will need ${calculate()} cans of paint to complete the job`);
+
+//Exercise 26
+const n = prompt("Check this number: ");
+
+function isPrime(number){
+    if (number === 2){
+        return true;
+    }else if (number % 2 === 0){
+        return false;
+    } 
+    const limit = Math.sqrt(number);
+    for (let i = 3; i <= limit; i += 2) { 
+        if (number % i === 0) {
+            return false
+        };
+    }
+    return true;
+}
+
+function primeChecker(number = n){
+    if (number <= 1 ){
+        console.log("Negative numbers can't be prime");
+    } else if (isPrime(number) === true ){
+        console.log("This number is prime");
+    } else {
+        console.log("This number is not prime");
+    }
+}
+primeChecker();
