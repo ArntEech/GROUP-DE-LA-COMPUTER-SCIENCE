81aec793398bfd2ecf5b39fab48a39ae80f07559
Yaro Williams <yarowilliams402@gmail.com>
2025-04-04 11:46:57 +0000

    added a few changes (#73)
    * Added a few comments
    
    * Daily Challenge2 Complted!
    
    * A small change
    
    * Added changes
    
    * Added another file for Daily Challenge 3
    
    * Daily Challenge 3 file
    
    * Day3 Challenges
    
    * Day 4 Challenges1
    
    * Update Day6.md
    
    * Day5 Challenges ompleted
    
    * Day6 Challenges Completed!
    
    * Projects 1,2,3,4 Completed
    
    * few changes

--- YaroWilliams/Project4.java
+++ YaroWilliams/Project4.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 public class Project4 {
@@ -17,9 +19,34 @@ public class Project4 {
         System.out.println("How many numbers will you like to include in your password? ");
         int numOfNumbers = input.nextInt();
 
-        String[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        String[] symbols = {'!', '#', '$', '%', '&', '(', ')', '*', '+'};
-        String[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+        String[] symbols = {"!", "#", "$", "%", "&", "(", ")", "*", "+"};
+
+        String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<=numOfLetters; i++){
+            // randomly get the index of the symbols array
+            int randomIndex = random.nextInt(symbols.length);
+            // append the symbol to the StringBuilder
+            sb.append(symbols[randomIndex]);
+        }
+        for(int i =0;i <=numOfNumbers; i++){
+            int randomIndex = random.nextInt(numbers.length);
+            sb.append(numbers[randomIndex]);
+        }
+        for(int i = 0; i<= numOfSymbols; i++){
+            int randomIndex = random.nextInt(symbols.length);
+            sb.append(symbols[randomIndex]);
+        }
+
+        List<String> charList = new ArrayList<>();
+        for (char c : sb.toString().toCharArray()) {
+            charList.add(String.valueOf(c));
+        }
     }
+
 }
