5969ac7f70376133c45a4df51f1a076192de2e1d
Roland Appiah <romappiah@st.ug.edu.gh>
2025-04-05 20:57:55 +0100

    Java script programming b4 (#71)
    * Day1 Exercise1
    
    * Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 1
    
    * Day 1 Exercise 1
    
    * git Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 4 solved
    
    * Day 2 Exercise 1 Solved
    
    * Day 2 Exercise 6
    
    * Exercise 9 solved
    
    * Exercise 10 solved
    
    * Exercise 11 Solved with a smile
    
    * "Day 3 exercise 12"
    
    * "Exercise 12"
    
    * Exercise 12 hmmmm
    
    * Exercise3 resolved with online compiler
    
    * Project 1 completed
    
    * PROJECT 2 added
    
    * done
    
    * Password Generator complete
    
    * Password Generator complete
    
    * Password Generator solved
    
    * Fixed issues with Rock paper scissors game
    
    * Altered
    
    * Exercise 13 solved
    
    * Day 4 completed
    
    * Day 5 firdst Exrcise
    
    * Exercise 17 done
    
    * Exercise 18 solved
    
    * Day 5 FizzBuzz complete
    
    * ...
    
    * Day 7
    
    * Prime number logic complete
    
    * Day 8 started ..
    
    * Day 8 Exercise 27 solved
    
    * Exercise 28 started
    
    ---------
    
    Co-authored-by: Arnt <Arnatech5@gmail.com>

--- Roland/Day8.js
+++ Roland/Day8.js
@@ -7,4 +7,37 @@ const scoresData = {
     Neville: 62
 };
 
-const ratingsData = scoresData.map()
+const ratingsData = {};
+
+for (const name in scoresData){
+    const score = scoresData[name];
+    let rating;
+
+    if (score > 90){
+        rating = "Outstanding";
+    } else if (80 <score<= 90){
+        rating = "Exceeds Expectations";
+    } else if (70 <score<= 80){
+        rating = "Acceptable";
+    } else if ( score < 70){
+        rating = "Fail";
+    }
+
+    ratingsData[name] = rating;
+}
+
+console.log(ratingsData);
+
+//Exercise 28
+const travel_log = {
+    [
+        country: France,
+        visits: 12,
+        cities: ["Paris", "Lille", "Dijon"]
+    ];
+    [
+        country: "Germany",
+        visits: 5,
+        cities : ["Berlin", "Hamburg", "Stuttgart"]
+    ];
+};
