281280ab114786eecfbc71398abf050c17483501
AdelaideOtoobeaQuaye <https://github.com/QuayeAdelaideOtoobea>
2025-04-07 11:54:41 +0000

    exercise1.js done

--- Adelaide/Basics/Code_Challenges/exercise1.js
+++ Adelaide/Basics/Code_Challenges/exercise1.js
@@ -2,4 +2,7 @@
 //'''
 //Instructions;
 
-console.log("Day 1 - Python Print Function"/n+"The function is declared like this:"/n+"print('what to print')");
+console.log("Day 1 - Output Function");  
+console.log("The function is declared like this:");  
+console.log("print('what to print')");
+
