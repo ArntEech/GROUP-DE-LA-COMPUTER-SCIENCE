b82d6cb4500b88ded914871c7fc1a9333d8aa51d
Roland Appiah <romappiah@st.ug.edu.gh>
2025-04-08 17:28:15 +0100

    Java script programming b4 (#79)
    * Day1 Exercise1
    
    * Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 1
    
    * Day 1 Exercise 1
    
    * git Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 4 solved
    
    * Day 2 Exercise 1 Solved
    
    * Day 2 Exercise 6
    
    * Exercise 9 solved
    
    * Exercise 10 solved
    
    * Exercise 11 Solved with a smile
    
    * "Day 3 exercise 12"
    
    * "Exercise 12"
    
    * Exercise 12 hmmmm
    
    * Exercise3 resolved with online compiler
    
    * Project 1 completed
    
    * PROJECT 2 added
    
    * done
    
    * Password Generator complete
    
    * Password Generator complete
    
    * Password Generator solved
    
    * Fixed issues with Rock paper scissors game
    
    * Altered
    
    * Exercise 13 solved
    
    * Day 4 completed
    
    * Day 5 firdst Exrcise
    
    * Exercise 17 done
    
    * Exercise 18 solved
    
    * Day 5 FizzBuzz complete
    
    * ...
    
    * Day 7
    
    * Prime number logic complete
    
    * Day 8 started ..
    
    * Day 8 Exercise 27 solved
    
    * Exercise 28 started
    
    * Project 5 started and put on hold
    
    ---------
    
    Co-authored-by: Arnt <Arnatech5@gmail.com>

--- Roland/Day8.js
+++ Roland/Day8.js
@@ -29,15 +29,3 @@ for (const name in scoresData){
 console.log(ratingsData);
 
 //Exercise 28
-const travel_log = {
-    [
-        country: France,
-        visits: 12,
-        cities: ["Paris", "Lille", "Dijon"]
-    ];
-    [
-        country: "Germany",
-        visits: 5,
-        cities : ["Berlin", "Hamburg", "Stuttgart"]
-    ];
-};
