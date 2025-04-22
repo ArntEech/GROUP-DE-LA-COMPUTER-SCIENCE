b96fe44aea72d4c0ceaea58fc48e65c3edef88e3
rolandhimself <romappiah@st.ug.edu.gh>
2025-04-22 14:40:25 +0100

    Hangman logic

--- Roland/Project_7/main.js
+++ Roland/Project_7/main.js
@@ -5,3 +5,16 @@ function getRandomWord() {
     const index = Math.floor(Math.random() * word_list.length);
     return word_list[index];
   }
+
+  function displayWord(displayArray) {
+    console.log(displayArray.join(' '));
+  }
+  
+  function hangman() {
+    console.log(logo);
+  
+    const chosenWord = getRandomWord();
+    const wordLength = chosenWord.length;
+    let display = Array(wordLength).fill('_');
+    let lives = 6;
+    let guessedLetters = new Set();}
