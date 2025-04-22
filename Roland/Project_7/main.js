d0f3e731f728204091c6086f17c52cc9b98d7606
rolandhimself <romappiah@st.ug.edu.gh>
2025-04-22 14:37:05 +0100

    Hangman Logic

--- Roland/Project_7/main.js
+++ Roland/Project_7/main.js
@@ -1,3 +1,7 @@
-import './HangmanWords.js';
-import './HangmanArt.js';
+import { word_list} from './HangmanWords.js';
+import { stages, logo} from './HangmanArt.js';
 
+function getRandomWord() {
+    const index = Math.floor(Math.random() * word_list.length);
+    return word_list[index];
+  }
