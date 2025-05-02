b75e5d845ce2583cbc57c2f981f71fa9b85fa896
rolandhimself <romappiah@st.ug.edu.gh>
2025-05-02 23:07:51 +0100

    commit

---
+++ Roland/Intermediate/Day10.js
@@ -0,0 +1,6 @@
+/*| Module Name      | Description                                                                 |
+|------------------|-----------------------------------------------------------------------------|
+| `coffee_maker`   | Handles resources and brewing logic                                         |
+| `menu`           | Contains the available drink options and their ingredients/cost            |
+| `money_machine`  | Handles coin processing and transactions                                    | */
+
