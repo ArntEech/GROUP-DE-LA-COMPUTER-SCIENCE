5df4753dde99aea19e5b68c379bf1f7ada8ecad7
Roland Appiah <romappiah@st.ug.edu.gh>
2025-05-06 17:10:07 +0100

    Java script programming b4 (#107)
    * Day1 Exercise1
    
    * Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 1
    
    * Day 1 Exercise 1
    
    * git Day 1 Exercise 2
    
    * Day 1 Exercise 3
    
    * Day 1 Exercise 4 solved
    
    * Day 2 Exercise 1 Solved
    
    * Day 2 Exercise 6
    
    * Exercise 9 solved
    
    * Exercise 10 solved
    
    * Exercise 11 Solved with a smile
    
    * "Day 3 exercise 12"
    
    * "Exercise 12"
    
    * Exercise 12 hmmmm
    
    * Exercise3 resolved with online compiler
    
    * Project 1 completed
    
    * PROJECT 2 added
    
    * done
    
    * Password Generator complete
    
    * Password Generator complete
    
    * Password Generator solved
    
    * Fixed issues with Rock paper scissors game
    
    * Altered
    
    * Exercise 13 solved
    
    * Day 4 completed
    
    * Day 5 firdst Exrcise
    
    * Exercise 17 done
    
    * Exercise 18 solved
    
    * Day 5 FizzBuzz complete
    
    * ...
    
    * Day 7
    
    * Prime number logic complete
    
    * Day 8 started ..
    
    * Day 8 Exercise 27 solved
    
    * Exercise 28 started
    
    * Project 5 started and put on hold
    
    * Mini-Project 5 solved with errors handledgracefull
    
    * Day 8 complete
    
    * uhvjh
    
    * Day 9 completed
    
    * Project 6 solved
    
    * Project 7 started
    
    * Day 9 modified
    
    * exported files and imported into main.js
    
    * Hangman Logic
    
    * Hangman logic
    
    * Folder created
    
    * commit
    
    * v hgvjh
    
    * YUVSJY
    
    * Day 11
    
    * OOP logic part1
    
    * Question module added
    
    * Data Module imported
    
    ---------
    
    Co-authored-by: Arnt <Arnatech5@gmail.com>

--- Roland/Intermediate/Exercise31-Day11/Data.js
+++ Roland/Intermediate/Exercise31-Day11/Data.js
@@ -1,4 +1,6 @@
+const questionData = [
 const question_data = [
+
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
@@ -11,4 +13,7 @@ const question_data = [
     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+export {questionData};
 ]
